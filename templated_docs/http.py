f8b675fcee220d4b568af50f6a2c451a7412f011
Curtis Maloney <curtis@tinbrain.net>
2017-06-08 11:08:24 +1000

    Use django.http.FileResponse

--- templated_docs/http.py
+++ templated_docs/http.py
@@ -3,21 +3,24 @@
 import os
 import mimetypes
 
-from django.http import HttpResponse
+from django.http import FileResponse as BaseFileResponse
 
 
-class FileResponse(HttpResponse):
+class DeleteOnClose:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def close(self):
+        os.unlink(self.filename)
+
+
+class FileResponse(BaseFileResponse):
     """
     One-time HTTP response with a generated file. DELETES A FILE AFTERWARDS!
     """
-    def __init__(self, actual_file, visible_name, delete=True,
-                 *args, **kwargs):
-        super(FileResponse, self).__init__(*args, **kwargs)
-
-        self['Content-type'] = mimetypes.guess_type(actual_file)[0]
+    def __init__(self, actual_file, visible_name, delete=True, *args, **kwargs):
+        kwargs.setdefault('content_type', mimetypes.guess_type(actual_file)[0])
+        super(FileResponse, self).__init__(open(actual_file, 'rb'), *args, **kwargs)
         self['Content-disposition'] = 'attachment; filename=%s' % visible_name
-        with open(actual_file, 'rb') as f:
-            self.content = f.read()
-        self['Content-length'] = len(self.content)
         if delete:
-            os.unlink(actual_file)
+            self._closable_objects.append(DeleteOnClose(actual_file))
